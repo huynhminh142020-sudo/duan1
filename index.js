e4d2bb0d17d7464b9dbc1471a991fa1a5471c5fc
huynhminh142020-sudo <huynhminh142020@gmail.com>
2025-11-02 11:56:02 +0700

    new codee

---
+++ index.js
@@ -0,0 +1,11 @@
+
+let age = 100;
+let fullName = "Huỳnh Minh";
+let email = "Huynhminh@gmail.com";
+let ngayTao = "02/11/2025";
+let ngayCapNhat = "02/11/2025";
+let ngayXoa = "10/10/2026";
+let str1 = "hello";
+let str2 = " helllo";
+let result = str1 + str2;
+console.log( result.length , age , fullName , email , ngayTao , ngayXoa , ngayCapNhat );
